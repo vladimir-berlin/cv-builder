9dca51eafbfc690d4d431cf469274e052d353639
Volodymyr Bezkorovainyi <volodymyr.bezkorovainyi@deutschebahn.com>
2025-11-16 20:02:44 +0700

    Minimize distance between bullet point list items

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,6 @@ SRC_NAME=CV_arch
 SRC=src/$SRC_NAME.md
 DST=generated/$SRC_NAME.docx
 
-TEMPLATE=templates/reference_clean_A4.docx
+TEMPLATE=templates/reference_clean_A4_lists_tight.docx
 
 pandoc $SRC --reference-doc=$TEMPLATE -f markdown+autolink_bare_uris -t docx -o $DST
