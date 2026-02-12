b0918e2c4b99d325021227b4232bd0486e7fc143
Vladimir Bezkorovainyi <vladimir.bezkorovainy@gmail.com>
2026-02-12 13:05:01 +0800

    Update generating template to tighten line spaces

--- scripts/generate_one.sh
+++ scripts/generate_one.sh
@@ -7,6 +7,8 @@ SRC_NAME="${SRC_FILE%.*}"
 
 DST=$BASE_PATH/generated/$SRC_NAME.docx
 PDF=$BASE_PATH/generated/$SRC_NAME.pdf
-TEMPLATE=$BASE_PATH/templates/reference_clean_A4_lists_tight.docx
+# TEMPLATE=$BASE_PATH/templates/reference_clean_A4_lists_tight.docx
+TEMPLATE=$BASE_PATH/templates/reference_template_spacing_adjusted.docx
+
 
 pandoc $SRC --reference-doc=$TEMPLATE -f markdown+autolink_bare_uris -t docx -o $DST
